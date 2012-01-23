a179a4c1996cc5f8809b50f470e834110e3ceb33
PhillipKregg <phillipkregg@gmail.com>
2012-01-23 16:17:44 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqueryMobile::Application.config.secret_token = 'c4ee2b5f07fd5f2e4b3dab468d96dc45daf6117981667923dfe8a14bb804fe8133968504008a8c458748608d246e1682d8401b10ed8bc06526394ec774da42be'
